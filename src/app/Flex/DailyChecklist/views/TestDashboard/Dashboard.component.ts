3ea5d34d83684431b80dd1d39b29019ffcbf042c
kachain jantalat <42669141+chainrocker55@users.noreply.github.com>
2020-02-27 17:02:54 +0700

    Create Drill down chart success

--- src/app/Flex/DailyChecklist/views/TestDashboard/Dashboard.component.ts
+++ src/app/Flex/DailyChecklist/views/TestDashboard/Dashboard.component.ts
@@ -30,7 +30,7 @@ export class Dashboard implements OnInit {
         this.InitialCombo();
         this.InitialScreen();
         this.InitialCriteria();
-        
+
     }
     criteriaPieChart: DashBoardPieChart = new DashBoardPieChart();
     comboShiftByLine: ComboIntValue[];
@@ -46,14 +46,14 @@ export class Dashboard implements OnInit {
         CODE: undefined,
         DISPLAY: 'All : - All -'
     };
-    InitialScreen(){
+    InitialScreen() {
         this.comboStringAllItem.DISPLAY = 'All : - All -';
         this.comboIntAllItem.DISPLAY = 'All : - All -';
     }
     InitialCriteria() {
         this.criteriaPieChart.SHIFTID = -1;
         this.criteriaPieChart.LINEID = -1;;
-      
+
     }
 
     InitialCombo() {
@@ -87,33 +87,78 @@ export class Dashboard implements OnInit {
         },
     ];
 
-
-
-
-
     typeChart = "Line Chart"
 
     public barChartOptions: ChartOptions = {
         responsive: true,
         // We use these empty structures as placeholders for dynamic theming.
         scales: { xAxes: [{}], yAxes: [{}] },
+        plugins: {
+            plugins: {
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'x',
+                        speed: 10,
+                        threshold: 10
+                    },
+                    zoom: {
+                        enabled: true,
+                        mode: 'y'
+                    }
+                }
+            }
+        }
     };
-    public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+
+    public barChartLabelsMounth: Label[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    public barChartLabelsDay: Label[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+    public barChartLabels: Label[] = this.barChartLabelsMounth;
     public barChartType: ChartType = 'bar';
     public barChartLegend = true;
 
+    // randomArray(length, max):Number[]{
+    //     var arr = new Array[length].map(e=>e+1*Math.round(Math.random() * max))
 
-    public barChartData: ChartDataSets[] = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'OK', backgroundColor: '#1E90FF',hoverBackgroundColor:'#63c2de' ,hoverBorderColor:'black',hoverBorderWidth:2, },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'NG', backgroundColor: 'red', hoverBackgroundColor: '#f86c6b' ,hoverBorderColor:'black', hoverBorderWidth:2},
-        { data: [9, 3, 4, 7, 5, 20, 1], label: 'NOT USED', backgroundColor: 'black', hoverBackgroundColor: '#73818f' ,hoverBorderColor:'black', hoverBorderWidth:2},
+    //     return arr
+    // }
+
+
+
+    public barChartData1: ChartDataSets[] = [
+        { data: this.RandomNumber(12, 80), label: 'OK', backgroundColor: '#1E90FF', hoverBackgroundColor: '#63c2de', hoverBorderColor: 'black', hoverBorderWidth: 2, },
+        { data: this.RandomNumber(12, 80), label: 'NG', backgroundColor: 'red', hoverBackgroundColor: '#f86c6b', hoverBorderColor: 'black', hoverBorderWidth: 2 },
+        { data: this.RandomNumber(12, 80), label: 'NOT USED', backgroundColor: 'black', hoverBackgroundColor: '#73818f', hoverBorderColor: 'black', hoverBorderWidth: 2 },
+    ];
+    public barChartData2: ChartDataSets[] = [
+        { data: this.RandomNumber(31 ,30), label: 'OK', backgroundColor: '#1E90FF', hoverBackgroundColor: '#63c2de', hoverBorderColor: 'black', hoverBorderWidth: 2, },
+        { data: this.RandomNumber(31, 30), label: 'NG', backgroundColor: 'red', hoverBackgroundColor: '#f86c6b', hoverBorderColor: 'black', hoverBorderWidth: 2 },
+        { data: this.RandomNumber(31, 30), label: 'NOT USED', backgroundColor: 'black', hoverBackgroundColor: '#73818f', hoverBorderColor: 'black', hoverBorderWidth: 2 },
     ];
     // events
-    public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-        //console.log(event, active);
+    public barChartData: ChartDataSets[] = this.barChartData1;
+    RandomNumber(length, max): number[] {
+        var arr = new Array<number>(length)
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = Math.round(Math.random() * max)
+        }
+        return arr
+    }
+
+    public chartClicked(e: any): void {
+
+        if (e.active[0] && e.active.length > 0) {
+            console.log(e.active[0]._index);
+            console.log(this.barChartData === this.barChartData1)
+            this.barChartData = this.barChartData === this.barChartData1 ? this.barChartData2 : this.barChartData1
+            this.barChartLabels = this.barChartLabels === this.barChartLabelsMounth ? this.barChartLabelsDay : this.barChartLabelsMounth
+            //this.barChartData = this.barChartData1
+
+        }
         // var a = [event,...active]
         // var b = a[1];
-        
+
+
 
 
     }
@@ -123,7 +168,7 @@ export class Dashboard implements OnInit {
     // }
 
     public randomize(): void {
-        
+
         this.typeChart = this.barChartType === 'bar' ? 'Line Chart' : 'Bar Chart';
         this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
     }
